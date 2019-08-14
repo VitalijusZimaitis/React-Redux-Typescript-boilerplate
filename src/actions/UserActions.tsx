450db78e9a3c8d819be75da61b639941217eb509
Vitalijus Zimaitis <vz@frontit.dk>
2019-08-14 13:23:14 +0300

    Simplified simple get call

--- src/actions/UserActions.tsx
+++ src/actions/UserActions.tsx
@@ -2,9 +2,5 @@ import { apiCall, asyncRequest } from "../generic/requests";
 import { UserGetAll } from "../types/User";
 
 export const getAllUsers = (callback?: (...args: any) => any) => {
-  return asyncRequest(
-    UserGetAll,
-    apiCall("GET", `/users`, null, false),
-    callback
-  );
+  return asyncRequest(UserGetAll, apiCall("GET", `/users`), callback);
 };
