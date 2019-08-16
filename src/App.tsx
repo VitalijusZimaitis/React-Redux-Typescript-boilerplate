ce10ef8aaa74f4abca405985a9f707c3298ad593
Vitalijus Zimaitis <vz@frontit.dk>
2019-08-16 15:10:20 +0300

    Added SASS support

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import UsersListContainer from "./containers/UsersListContainer";
 import { BrowserRouter, Route } from "react-router-dom";
+import "./styles/css/App.css";
 
 const App: React.FC = () => {
   return (
