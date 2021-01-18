c8295584cb8dac420817299b1bf8942ce6af2f97
Vitalijus Zimaitis <vz@frontit.dk>
2021-01-18 13:33:08 +0200

    Folder structure tweaked

--- src/containers/Routes/AppRoutes.tsx
+++ src/containers/Routes/AppRoutes.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import RouteManager from "../RouteManager/RouteManager";
+import RouteManager from "./RouteManager";
 import { Routes } from "../../Routes";
 
 const AppRoutes: React.FC = (): JSX.Element => {
